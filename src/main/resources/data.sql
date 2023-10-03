837a1558476eada2a285a33275eeace48e11abc2
Isa Mary <isa.kusuki@gmail.com>
2023-10-02 23:39:52 -0300

    Criação do Projeto

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TASK (title, description, score, status) VALUES ('Prototipo', 'Criar o prototipo no figma', 50, 40);
+INSERT INTO TASK (title, description, score) VALUES ('DB', 'Modelar o banco de dados no site', 30);
+INSERT INTO TASK (title, description, score) VALUES ('Testes', 'Criar a base de testes', 20);
